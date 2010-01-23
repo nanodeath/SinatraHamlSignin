be2525cd7107dc249b63388106be33cc99efef98
Max Aller <nanodeath@gmail.com>
2010-01-23 14:03:32 -0800

    Username, haml, correct sessions.

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 
-use :sessions, true
+enable :sessions
 
 helpers do
   def validate(username, password)
@@ -16,10 +17,22 @@ helpers do
   def clear_session
     session.clear
   end
+  
+  def the_user_name
+    if is_logged_in? 
+      session["username"] 
+    else
+      "not logged in"
+    end
+  end
 end
 
 get '/' do
   haml :index
+end
+
+get '/about' do
+  haml :about
 end
 
 get '/login' do
@@ -28,12 +41,14 @@ end
 
 post '/login' do
   if(validate(params["username"], params["password"]))
+    puts "yay"
     session["logged_in"] = true
     session["username"] = params["username"]
     @message = "You've been logged in.  Welcome back, #{params["username"]}"
     haml :index
   else
-    @errors = "Sorry, those credentials aren't valid."
+    puts "error"
+    @error_message = "Sorry, those credentials aren't valid."
     haml :login
   end
 end
